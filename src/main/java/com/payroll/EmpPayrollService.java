dc982a30a64f228f533dcea17e9cb50fe1c56242
ritika-saha <ritika882001@gmail.com>
2023-12-06 21:51:22 +0530

    writing to file

--- src/main/java/com/payroll/EmpPayrollService.java
+++ src/main/java/com/payroll/EmpPayrollService.java
@@ -1,5 +1,10 @@
 package com.payroll;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -20,8 +25,26 @@ public class EmpPayrollService {
         payrollList.add(new EmpPayroll(id, name, salary));
     }
 
-    private void writeEmployeePayrollData(EmpPayroll employeePayroll) {
-        System.out.println("Employee Payroll Data: " + employeePayroll);
+    private void writeEmployeePayrollData(String path) {
+       try(BufferedWriter writer=new BufferedWriter(new FileWriter(path))){
+        for(EmpPayroll data : payrollList){
+            writer.write(data.toString()+"\n");
+        }
+        System.out.println("Data Written to file");
+       }catch(IOException e){
+        e.printStackTrace();
+       }
+    }
+
+    public int countEntries(String path){
+        int entryCount = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            while (reader.readLine() != null) 
+            entryCount++;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return entryCount;
     }
 
     public static void main(String[] args) {
@@ -29,6 +52,12 @@ public class EmpPayrollService {
         service.readEmployeePayrollData();
         service.readEmployeePayrollData();
         service.readEmployeePayrollData();
-        
+        String path="PayrollData.txt";
+        service.writeEmployeePayrollData(path);
+        System.out.println("-------------------------- following data -------------------------------");
+        System.out.println(service.payrollList);
+        System.out.println("-------------------------------------------------------------------------");
+        int entryCount=service.countEntries(path);
+        System.out.println("Number of Employee payroll entries : "+entryCount);
     }
 }
